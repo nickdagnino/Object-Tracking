301cf3513bd72305f4f4dd745d188f36a4775631
Nick Dagnino <84521663+nickdagnino@users.noreply.github.com>
2024-09-09 10:15:39 -0400

    Create n-pt_tracking.py

---
+++ n-pt_tracking.py
@@ -0,0 +1,12 @@
+# Given: defined number n points equadistance of known distance from centroid in plane parallel to camera plane
+# Find: location of centroid in three-dimensional Euclidian space
+# Return: record of position of centroid
+
+# Step 1: Initialize Camera
+# Step 2: Calibrate Camera w/ system
+# Step 3: Initialize Record
+# Step 4: Determine Start -> start record
+# Step 5: Determine location of centriod
+# Step 6: Add determined location to record
+# Step 7: Determine End -> end record
+# Step 8: Save Record
